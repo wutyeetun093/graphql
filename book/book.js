149c27a7c4c46a69130f49379ba644e2bd08b039
Wut Yee Tun <wutyeetun@achromex.com>
2019-12-07 11:59:14 +0630

    book tutorial

--- book/book.js
+++ book/book.js
@@ -9,10 +9,16 @@ const {
   GraphQLList
 } = graphql;
 const books = [
-  { name: "The river", genre: "Fantasy", id: "1" },
-  { name: "Monkey", genre: "Si=Fi", id: "2" },
-  { name: "Apple Inc", genre: "Biography", id: "3" },
-  { name: "Steve Jobs", genre: "Biography", id: "4" }
+  { name: "The river", genre: "Fantasy", id: "1", authorId: "1" },
+  { name: "Monkey", genre: "Si=Fi", id: "2", authorId: "1" },
+  { name: "Apple Inc", genre: "Biography", id: "3", authorId: "2" },
+  { name: "Steve Jobs", genre: "Biography", id: "4", authorId: "3" }
+];
+const authors = [
+  { name: "Mg Mg", age: "26", id: "1" },
+  { name: "Su Su", age: "27", id: "2" },
+  { name: "Hla Hla", age: "30", id: "3" },
+  { name: "Mg Ba", age: "50", id: "4" }
 ];
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -22,6 +28,22 @@ const BookType = new GraphQLObjectType({
     genre: { type: GraphQLString }
   })
 });
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return _.filter(books, { authorId: parent.id });
+      }
+    }
+  })
+});
+
 const RootQuery = new GraphQLObjectType({
   name: "RootQuery",
   fields: {
@@ -35,9 +57,33 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         return _.find(books, { id: args.id });
       }
+    },
+    author: {
+      type: AuthorType,
+      args: {
+        id: {
+          type: GraphQLID
+        }
+      },
+      resolve(parent, args) {
+        return _.find(authors, { id: args.id });
+      }
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return books;
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return authors;
+      }
     }
   }
 });
+
 module.exports = new GraphQLSchema({
   query: RootQuery
   // mutation: Mutation
